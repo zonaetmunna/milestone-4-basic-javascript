41f0a29d27e09875ee6f40a7ca6b74ad43caf516
zonaetmunna <zonaetmonna@gmail.com>
2022-04-09 19:45:23 +0600

    add

---
+++ mathmatical_operation.js
@@ -0,0 +1,29 @@
+// addition
+var a=12;
+var b=12;
+var sum=a+b;
+console.log(sum);
+
+// subtraction
+var num1=12;
+var num2=12;
+var sub=num1-num2;
+console.log(sub);
+
+// multiplication
+var num3=12;
+var num4=12;
+var mul=num3*num4;
+console.log(mul);
+
+// division
+var num5=12;
+var num6=12;
+var div=num5/num6;
+console.log(div);
+
+// modulus
+var num7=12;
+var num8=12;
+var mod=num7%num8;
+console.log(mod);
